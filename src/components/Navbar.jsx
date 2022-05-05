75450a0e6358b3ca27a4cccee6eacb192a23662b
David Ogar <owogogahhero@outlook.com>
2022-05-05 13:51:42 +0100

    building the Navabar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2> FUPRE SDGS</h2>
+            <p> welcome </p>
+        </div>
+    )
+}
+
+export default Navbar;
