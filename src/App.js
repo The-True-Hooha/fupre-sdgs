adc6c570d071c65f253ef724c832ee08bf495e21
David Ogar <owogogahhero@outlook.com>
2022-05-06 08:10:38 +0100

    navbar working

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <Navbar/>
+    <div>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import {AiOutlineClose} from 'react-icons/ai';
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2> FUPRE SDGS</h2>
-            <p> welcome </p>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black">
+      <h1 className="w-full text-3xl font-bold text-[#000000]"> FUPRE SDGS </h1>
+      <ul className="flex">
+        <li className="p-8 font-bold"> Home </li>
+        <li className="p-8 font-bold"> About </li>
+        <li className="p-8 font-bold"> Contact </li>
+        <li className="p-8 font-bold"> Services </li>
+        <li className="p-8 font-bold"> Vision </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
